cec69d16479395e2842ba43a253b9256b461b658
lang0909 <lang0909@ajou.ac.kr>
2019-09-23 23:20:56 +0900

    Modify HttpConfig

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/config/HttpConfig.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/config/HttpConfig.java
@@ -1,4 +1,14 @@
 package org.LeeAn.fifaonline4.fifaonline4.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class HttpConfig {
+
+    @Bean
+    public RestTemplate createRestTemplate(){
+        return new RestTemplate();
+    }
 }
