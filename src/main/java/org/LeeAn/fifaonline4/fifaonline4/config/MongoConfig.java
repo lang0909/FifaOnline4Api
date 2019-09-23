e9a5222070244a45f1f7e6c2536b3dea04d04b9f
lang0909 <lang0909@ajou.ac.kr>
2019-09-23 23:24:46 +0900

    Modify MongoConfig

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/config/MongoConfig.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/config/MongoConfig.java
@@ -1,4 +1,16 @@
 package org.LeeAn.fifaonline4.fifaonline4.config;
 
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
 public class MongoConfig {
+
+    @Bean
+    public MongoTemplate createMongoTemplate(){
+        return new MongoTemplate(new MongoClient(), "fifaonline4-api");
+    }
 }
