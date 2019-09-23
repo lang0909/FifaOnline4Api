ecce0b484b5707297cd1cf74e80d06fbc05222f1
lang0909 <lang0909@ajou.ac.kr>
2019-09-23 23:25:47 +0900

    Modify FifaOnline4Application

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/FifaOnline4Application.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/FifaOnline4Application.java
@@ -2,8 +2,10 @@ package org.LeeAn.fifaonline4.fifaonline4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FifaOnline4Application {
 
 	public static void main(String[] args) {
