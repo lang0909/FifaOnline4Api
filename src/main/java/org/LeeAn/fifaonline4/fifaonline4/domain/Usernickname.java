2dca1c50512dcfb1d8349243e4fb08ee498b4927
lang0909 <lang0909@ajou.ac.kr>
2019-09-24 20:22:52 +0900

    Add Usernickname.java

---
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/domain/Usernickname.java
@@ -0,0 +1,10 @@
+package org.LeeAn.fifaonline4.fifaonline4.domain;
+
+import lombok.Data;
+
+@Data
+public class Usernickname {
+    private String accessId;
+    private String nickname;
+    private int level;
+}
