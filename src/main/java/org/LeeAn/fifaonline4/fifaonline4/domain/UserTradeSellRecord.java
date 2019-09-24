a6bf02fae5a2dfb87c470e861eb7a06151c120da
lang0909 <lang0909@ajou.ac.kr>
2019-09-24 21:46:21 +0900

    Modify UserTradeSellRecord.java

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/domain/UserTradeSellRecord.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/domain/UserTradeSellRecord.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class UserTradeSellRecord {
     private String tradeDate;
     private String saleSn;
-    private int spid;
+    private int spId;
     private int grade;
     private int value;
 }
