a00bab5578e0eb2d97b1d523e8080c19b2e773ec
lang0909 <lang0909@ajou.ac.kr>
2019-09-24 20:42:59 +0900

    Add UserTradeBuyRecord.java

---
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/domain/UserTradeBuyRecord.java
@@ -0,0 +1,12 @@
+package org.LeeAn.fifaonline4.fifaonline4.domain;
+
+import lombok.Data;
+
+@Data
+public class UserTradeBuyRecord {
+    private final String tradeDate;
+    private final String saleSn;
+    private final int spid;
+    private final int grade;
+    private final int value;
+}
