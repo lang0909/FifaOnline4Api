7a5011c7466012ae0d707f8ce451ece46f8bbca2
lang0909 <lang0909@ajou.ac.kr>
2019-09-24 20:33:42 +0900

    Add Maxdivision.java

---
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/domain/Maxdivision.java
@@ -0,0 +1,10 @@
+package org.LeeAn.fifaonline4.fifaonline4.domain;
+
+import lombok.Data;
+
+@Data
+public class Maxdivision {
+    private int matchType;
+    private int division;
+    private String achievementDate;
+}
