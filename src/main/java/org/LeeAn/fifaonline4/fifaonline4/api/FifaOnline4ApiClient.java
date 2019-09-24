2fa62e364187235663687bc92c703f26e2908402
lang0909 <lang0909@ajou.ac.kr>
2019-09-24 23:14:15 +0900

    Modify FifaOnline4ApiClient.java

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/api/FifaOnline4ApiClient.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/api/FifaOnline4ApiClient.java
@@ -1,8 +1,12 @@
 package org.LeeAn.fifaonline4.fifaonline4.api;
 
 
-import org.LeeAn.fifaonline4.fifaonline4.domain.Usernickname;
+import com.fasterxml.jackson.databind.deser.DataFormatReaders;
+import org.LeeAn.fifaonline4.fifaonline4.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 public class FifaOnline4ApiClient {
     private final String user_nickname_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={nickname}";
     private final String user_accessid_maxdivison_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}/maxdivision";
-    private final String user_matchtype_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}/matches?matchtype={matchtype}&offset={offset}&limit={limit}";
     private final String user_tradetype_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}/markets?tradetype={tradetype}&offset={offset}&limit={limit}";
     private final String user_matchdetail_url = "https://api.nexon.co.kr/fifaonline4/v1.0/matches/{matchid}";
     private final String top10000_ranker_use_url = "https://api.nexon.co.kr/fifaonline4/v1.0/rankers/status?matchtype={matchtype}&players={players}";
@@ -22,6 +25,50 @@ public class FifaOnline4ApiClient {
     RestTemplate restTemplate;
 
     public Usernickname requestUser(String user_nickname){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
 
+        return restTemplate.exchange(user_nickname_url, HttpMethod.GET,entity,Usernickname.class,user_nickname).getBody();
+    }
+
+    public Maxdivision requestMaxDivision(String accessid) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(user_accessid_maxdivison_url,HttpMethod.GET,entity,Maxdivision.class,accessid).getBody();
+    }
+
+    public UserTradeBuyRecord requestTradeBuyRecord(String accessid,String b_s, int offSet, int limit){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(user_tradetype_url,HttpMethod.GET,entity,UserTradeBuyRecord.class,accessid,b_s,offSet,limit).getBody();
+    }
+
+    public UserTradeSellRecord requestTradeSellRecord(String accessid,String b_s, int offSet, int limit){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(user_tradetype_url,HttpMethod.GET,entity,UserTradeSellRecord.class,accessid,b_s,offSet,limit).getBody();
+    }
+
+    public MatchDetail requestMatchDetail(String matchid){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(user_matchdetail_url,HttpMethod.GET,entity,MatchDetail.class,matchid).getBody();
+    }
+
+    public TopRankerUsingAverage requestTopRankerUsingAverage(int matchtype,String players){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",api_key);
+        final HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(top10000_ranker_use_url,HttpMethod.GET,entity,TopRankerUsingAverage.class,matchtype,players).getBody();
     }
 }
