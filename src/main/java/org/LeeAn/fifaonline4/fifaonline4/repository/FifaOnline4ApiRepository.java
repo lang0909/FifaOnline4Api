b33750d46d50a345fe2615dd9f30c5b2221fc155
lang0909 <lang0909@ajou.ac.kr>
2019-09-25 13:50:59 +0900

    Modify FifaOnline4ApiRepository.java

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/repository/FifaOnline4ApiRepository.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/repository/FifaOnline4ApiRepository.java
@@ -1,4 +1,20 @@
 package org.LeeAn.fifaonline4.fifaonline4.repository;
 
+import org.LeeAn.fifaonline4.fifaonline4.domain.UserTradeSellRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class FifaOnline4ApiRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void insertUserTradeSellRecord(UserTradeSellRecord[] userTradeSellRecords){
+        for(int i=0;i<userTradeSellRecords.length;i++)
+        {
+            mongoTemplate.insert(userTradeSellRecords[i]);
+        }
+    }
 }
