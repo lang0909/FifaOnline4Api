e6e88ce324644d802b4f5390858826019aeea116
lang0909 <lang0909@ajou.ac.kr>
2019-09-25 13:51:38 +0900

    Modify FifaOnline4ApiService.java

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/service/FifaOnline4ApiService.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/service/FifaOnline4ApiService.java
@@ -1,4 +1,30 @@
 package org.LeeAn.fifaonline4.fifaonline4.service;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.LeeAn.fifaonline4.fifaonline4.api.FifaOnline4ApiClient;
+import org.LeeAn.fifaonline4.fifaonline4.domain.UserTradeSellRecord;
+import org.LeeAn.fifaonline4.fifaonline4.domain.Usernickname;
+import org.LeeAn.fifaonline4.fifaonline4.repository.FifaOnline4ApiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class FifaOnline4ApiService {
+
+    @Autowired
+    FifaOnline4ApiClient fifaOnline4ApiClient;
+
+    @Autowired
+    FifaOnline4ApiRepository fifaOnline4ApiRepository;
+
+    @Scheduled(fixedDelay = 10000L)
+    public void getFifaOnline4Api(){
+        Usernickname usernickname = fifaOnline4ApiClient.requestUser("아주대학교");
+        UserTradeSellRecord[] userTradeSellRecord =  fifaOnline4ApiClient.requestTradeSellRecord(usernickname.getAccessId(),"sell",0,20);
+        fifaOnline4ApiRepository.insertUserTradeSellRecord(userTradeSellRecord);
+        log.info("fifaonline4 api has been inserted successfully. {}", userTradeSellRecord);
+    }
 }
