f87802835e60d98cb3891c0a7b77cc320ccb24e9
lang0909 <lang0909@ajou.ac.kr>
2019-12-23 00:46:44 +0900

    modify api key

--- src/main/java/org/LeeAn/fifaonline4/fifaonline4/api/FifaOnline4ApiClient.java
+++ src/main/java/org/LeeAn/fifaonline4/fifaonline4/api/FifaOnline4ApiClient.java
@@ -18,9 +18,7 @@ public class FifaOnline4ApiClient {
     private final String user_tradetype_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users/{accessid}/markets?tradetype={tradetype}&offset={offset}&limit={limit}";
     private final String user_matchdetail_url = "https://api.nexon.co.kr/fifaonline4/v1.0/matches/{matchid}";
     private final String top10000_ranker_use_url = "https://api.nexon.co.kr/fifaonline4/v1.0/rankers/status?matchtype={matchtype}&players={players}";
-    private final String api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiODIyMjE4NjgzIiwiYXV0aF9pZCI6IjIiLCJ0b2" +
-            "tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjIwMDAwOjEwIiwibmJmIjox" +
-            "NTY3MDkxNjMzLCJleHAiOjE2MzAxNjM2MzMsImlhdCI6MTU2NzA5MTYzM30.rcv7fksQgEq-DElMRcUJAYEndUMjmLQU7OKjGpH5gVE";
+    private final String api_key = "";
 
     @Autowired
     RestTemplate restTemplate;
